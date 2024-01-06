4f100f6733d0bba0813aed1393d4f48e8fbc8ce0
Abdullah Al Hadi <abdullah.hadi@cefalo.com>
2024-01-06 18:40:50 +0600

    Project added
    Signed-off-by: Abdullah Al Hadi <abdullah.hadi@cefalo.com>

---
+++ TrendingMovies-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrendingMovies-SwiftUI
+//
+//  Created by Abdullah Al Hadi on 6/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrendingMovies-SwiftUI/TrendingMovies_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingMovies_SwiftUIApp.swift
+//  TrendingMovies-SwiftUI
+//
+//  Created by Abdullah Al Hadi on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TrendingMovies_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
